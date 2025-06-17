42c2400c022e2b64e3702abf0f639cb3bd9a592b
Szymon Wilczek <kazikwilczek7@gmail.com>
2025-06-17 18:36:01 +0200

    Update EasterEggPage.tsx

--- projekt-angielski/src/pages/easter-egg/EasterEggPage.tsx
+++ projekt-angielski/src/pages/easter-egg/EasterEggPage.tsx
@@ -8,8 +8,8 @@ function EasterEggPage() {
         <h2 className="text-3xl font-bold text-red-400 font-mono">
           Forbidden Page Mate!
         </h2>
-        <Link 
-          to="/" 
+        <Link
+          to="/"
           className="inline-block bg-neutral-700 hover:bg-neutral-800 border border-neutral-600 font-mono font-bold text-white px-6 py-2 rounded-lg transition-colors duration-200"
         >
           Back to Page 1
